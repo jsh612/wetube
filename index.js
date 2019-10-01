512327745bd324c7d61c96848bc57d660d8cc106
seungho Jeon <urijsh612@gmail.com>
2019-10-01 18:25:57 +0900

    기초적인 express 서버 구성해보기

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on Http://localhost:${PORT}`);
+}
+
+const handleHoem = (req, res) => {
+    console.log(req)
+    res.send('Hello from home');
+}
+
+const handleProfile = (req, res) => {
+    res.send('You are on my profile');
+}
+
+app.get('/', handleHoem);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
