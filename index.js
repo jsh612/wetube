704304d143f1f3b31db6b26a8f614ffdb9edd177
seungho Jeon <urijsh612@gmail.com>
2019-10-02 15:22:19 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const handleListening = () => {
 }
 
 const handleHoem = (req, res) => {
-    console.log(req)
     res.send('Hello from home');
 }
 
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+const betweenHome = (req, res, next) => {
+    console.log('I am beteen');
+    next()//next를 통해 다음 미들웨어로 넘긴다.   
+}
 
-app.get('/', handleHoem);
+app.use(betweenHome)//모든 path에서 미들웨어 작동
+
+app.get('/', betweenHome, handleHoem); // 해당 path에서만 미들웨어 자곧ㅇ
 
 app.get('/profile', handleProfile);
 
