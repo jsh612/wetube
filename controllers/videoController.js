fe48685aa48aec21421505fec01f8bb6f83262ae
seungho Jeon <urijsh612@gmail.com>
2019-10-04 18:19:49 +0900

    #2.9-11 MVC Pattern

---
+++ controllers/videoController.js
@@ -0,0 +1,8 @@
+export const videoHome =  (req, res) => res.send('home');
+export const searchVideos = (req, res) => res.send('Search');
+export const videos = (req, res) => res.send('videos');
+export const upload = (req, res) => res.send('upload');
+export const videoDetail = (req, res) => res.send('video detail');
+export const editVideo = (req, res) => res.send('edit video');
+export const deleteVideo = (req, res) => res.send('delete video');
+
