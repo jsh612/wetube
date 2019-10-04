97f6de420b3b3378a83c222394128afca7a8d1fa
seungho Jeon <urijsh612@gmail.com>
2019-10-04 18:16:46 +0900

    #2.8-11 MVC Pattern

---
+++ routers/globalRouter.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from '../routes';
+import { videoHome, searchVideos } from '../controllers/videoController';
+import { join, login, logout } from '../controllers/userController';
+
+const globalRouter = express.Router()
+
+globalRouter.get(routes.home, videoHome);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, searchVideos);
+
+export default globalRouter;
---
+++ routes.js
@@ -0,0 +1,51 @@
+import app from "./app";
+
+//- 이곳에 Router들이 사용 할 수 있도록 url들을 모아둔다
+//- router들은 이곳에서 url을 가져다 쓴다.
+//- 이와같이하는 이유는 동잃나 url을 여로 곳에서 쓸 경우 효율적으로 사용하기위함이다.
+
+
+//GLobal
+const HOME = '/';
+const JOIN = '/join';
+const LOGIN = '/login';
+const LOGOUT = '/logout';
+const SEARCH = '/search';
+
+
+//Users
+const USERS = '/users';
+const USER_DETAIL = '/:id';
+const EDIT_PROFILE = '/edit-profile';
+const CHANGE_PASSWORD = '/change-password';
+
+
+//Videos
+const VIDEOS = '/videos';
+const UPLOAD = '/upload';
+const VIDEO_DETAIL = '/:id';
+const EDIT_VIDEO = '/:id/edit';
+const DELETE_VIDEO = '/:id/delete';
+
+
+const routes = {
+    //global
+    home: HOME,
+    join: JOIN,
+    login: LOGIN,
+    logout: LOGOUT,
+    search: SEARCH,
+    //users
+    users: USERS,
+    userDetail: USER_DETAIL,
+    editProfile: EDIT_PROFILE,
+    changePassword: CHANGE_PASSWORD,
+    //videos
+    videos: VIDEOS,
+    upload: UPLOAD,
+    videoDetail: VIDEO_DETAIL,
+    editVideo: EDIT_VIDEO,
+    deleteVideo: DELETE_VIDEO
+}
+
+export default routes;
