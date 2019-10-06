0e2dd938edcd2c1c224ce255e42d504f69228f2e
seungho Jeon <urijsh612@gmail.com>
2019-10-06 18:03:09 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,17 @@
 export const videoHome =  (req, res) => res.render('home', {pageTitle: "Home"});
 //- 위의 코드 설명
 //  pageTitle 변수가 home 템플릿으로 전달된다.
-export const searchVideos = (req, res) => res.render('search', {pageTitle: "Search"});
+export const searchVideos = (req, res) => {
+  const {query: {term:searchingBy}} = req 
+  console.log("작성결과:::", searchingBy)
+  res.render("search", { pageTitle: "Search", searchingBy});
+}
+
 export const upload = (req, res) => res.render('upload', {pageTitle: "Upload"});
+
 export const videoDetail = (req, res) => res.render('videoDetail', {pageTitle: "video detail"});
+
 export const editVideo = (req, res) => res.render('editVideo', {pageTitle: "edit video"});
+
 export const deleteVideo = (req, res) => res.render('deleteVideo', {pageTitle: "eelete video"});
 
