ee13af1435f4479fd69b6c426881de096ada8cb8
seungho Jeon <urijsh612@gmail.com>
2019-10-07 15:48:25 +0900

    #2.21-22 Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,12 @@
-export const videoHome =  (req, res) => res.render('home', {pageTitle: "Home"});
+import {videos} from '../db'
+export const videoHome =  (req, res) => {
+  res.render('home', {pageTitle: "Home", videos});
+}
+
 //- 위의 코드 설명
 //  pageTitle 변수가 home 템플릿으로 전달된다.
 export const searchVideos = (req, res) => {
   const {query: {term:searchingBy}} = req 
-  console.log("작성결과:::", searchingBy)
   res.render("search", { pageTitle: "Search", searchingBy});
 }
 
