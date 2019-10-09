676d93f9d55e7e61ede1313d6a2157960f3eb576
seungho Jeon <urijsh612@gmail.com>
2019-10-09 23:15:50 +0900

    #3.9 Editing a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,22 +2,31 @@ import express from 'express';
 import routes from '../routes';
 import { 
   videoDetail,
-  editVideo,
   deleteVideo,
   getUpload,
-  postUpload
+  postUpload,
+  getEditVideo,
+  postEditVideo
 } from '../controllers/videoController';
 import { uploadVideo } from '../middlewares';
 
 const videoRouter = express.Router()
 
+// upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 //우리가 file을 업로드하면 uploadVidoe 함수를 통해 server에 있는 폴더(여기서는 video/)에 업로드되고,
 //postUpload라는 함수는 해당 file에 접근할 것이다.
 
+
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 
