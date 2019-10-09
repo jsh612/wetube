f70830d44e601e5f2c10336794ab6fb23d6262a8
seungho Jeon <urijsh612@gmail.com>
2019-10-09 15:24:19 +0900

    #3.5 Home Controller Finished - 모델 연결

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,22 @@
 import routes from '../routes';
-export const videoHome =  (req, res) => {
-  res.render('home', {pageTitle: "Home", videos});
+import Video from '../models/Video';
+
+
+export const home =  async (req, res) => {
+  //  try/catch문으로 에러잡기
+  try {
+    const videos = await Video.find({});
+    res.render('home', {pageTitle: "Home", videos}); 
+  } catch (error) {
+    console.log(error);
+    //에러가 발생한경우 videos는 빈 배열로 실행 시킨다.(즉, 비디오가 없는 상태로 render)
+    res.render('home', {pageTitle: "Home", videos: []}); 
+  }
 }
 
 //- 위의 코드 설명
 //  pageTitle 변수가 home 템플릿으로 전달된다.
-export const searchVideos = (req, res) => {
+export const search = (req, res) => {
   const {query: {term:searchingBy}} = req 
   res.render("search", { pageTitle: "Search", searchingBy, videos});
 }
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import routes from '../routes';
-import { videoHome, searchVideos } from '../controllers/videoController';
+import { home, search } from '../controllers/videoController';
 import { getJoin, postJoin, getLogin, postLogin, logout } from '../controllers/userController';
 
 const globalRouter = express.Router()
@@ -8,12 +8,12 @@ const globalRouter = express.Router()
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
-globalRouter.get(routes.home, videoHome);
+globalRouter.get(routes.home, home);
 
 globalRouter.get(routes.login, getLogin);
 globalRouter.post(routes.login, postLogin);
 
 globalRouter.get(routes.logout, logout);
-globalRouter.get(routes.search, searchVideos);
+globalRouter.get(routes.search, search);
 
 export default globalRouter;
