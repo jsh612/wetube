1e527cd9102a6df90b876c8b3b4d3647b4891887
seungho Jeon <urijsh612@gmail.com>
2019-10-21 17:29:41 +0900

    #6.12 Facebook Log In part Three

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,7 +81,28 @@ export const facebookLoginCallback = async (
   profile,
   cb
 ) => {
-  console.log("페푹:::", profile);
+  const {
+    _json: { id, name, email }
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.facebookId = id;
+      user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      facebookId: id,
+      avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    console.log(error);
+    return cb(error);
+  }
 };
 
 export const postFacbookLogin = (req, res) => {
