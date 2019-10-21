1715f7b2bd7bcf4a079ff91a29e8a26298f94b8e
seungho Jeon <urijsh612@gmail.com>
2019-10-21 16:57:49 +0900

    #6.11 Facebook Login Part Two

--- passport.js
+++ passport.js
@@ -43,7 +43,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://tiny-duck-58.localtunnel.me${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
