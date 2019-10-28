8430408543e5b9462ce942ac283d9c55cfe9e69c
seungho Jeon <urijsh612@gmail.com>
2019-10-28 14:03:41 +0900

    #10.1 API Registering a View part Two

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -93,6 +93,7 @@ function setTotalTime() {
 }
 
 function handleEnded() {
+  registerView(); // 해당 영사 종료시 조회수 +1
   videoPlayer.currentTime = 0;
   playBtn.innerHTML = '<i class="fas fa-play"></i>';
 }
@@ -126,3 +127,13 @@ if (videoContainer) {
   // 해당 사항을 체크하여 이벤트를 부여한다.
   init();
 }
+
+// 조회수와 관련된 것들( videoController.js / postRegisterView 와 연관)
+
+const registerView = () => {
+  const videoId = window.location.href.split("/videos/")[1]; // 해당 video의 id 값을 가져오게된다
+  fetch(`/api/${videoId}/view`, {
+    // database의 자료(views) 를 변경해야 하므로 POST ( GET X )
+    method: "POST"
+  });
+};
